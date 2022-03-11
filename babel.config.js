11fc317a558594cc537cc9fdb90244810d116a61
Raí Adriano <raivieiraadriano92@gmail.com>
2022-03-11 14:46:50 -0300

    added src path alias

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,17 @@ module.exports = function (api) {
   api.cache(true)
 
   return {
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            src: './src'
+          }
+        }
+      ]
+    ],
     presets: ['babel-preset-expo']
   }
 }
