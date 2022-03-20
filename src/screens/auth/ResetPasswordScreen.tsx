c416903dc604e773b184b0a7a621c2c1b304969f
Raí Adriano <raivieiraadriano92@gmail.com>
2022-03-20 16:17:34 -0300

    feat: implemented reset password screen ui

--- src/screens/auth/ResetPasswordScreen.tsx
+++ src/screens/auth/ResetPasswordScreen.tsx
@@ -1,4 +1,41 @@
-import { View } from 'react-native'
+import { useCallback } from 'react'
+
+import { Button, FormControl, Heading, Input, ScrollView, Text, VStack } from 'native-base'
 import { AuthStackScreenComponent } from 'src/navigation'
 
-export const ResetPasswordScreen: AuthStackScreenComponent<'ResetPassword'> = () => <View />
+export const ResetPasswordScreen: AuthStackScreenComponent<'ResetPassword'> = ({ navigation }) => {
+  const onSubmit = useCallback(() => {
+    navigation.navigate('ResetPasswordConfirmation')
+  }, [navigation])
+
+  return (
+    <ScrollView _contentContainerStyle={{ flexGrow: 1, p: 6 }}>
+      <VStack flex={1} safeAreaBottom space={10}>
+        <VStack space={4}>
+          <Heading fontSize="2xl" fontWeight="semibold" lineHeight="md">
+            Reset password
+          </Heading>
+          <Text fontSize="sm" fontWeight="normal" lineHeight="lg" _light={{ opacity: 0.6 }}>
+            Enter the email associated with your account and we’ll send and email with instructions
+            to reset your password.
+          </Text>
+        </VStack>
+        <VStack flex={1} justifyContent="space-between" space={10}>
+          <FormControl>
+            <FormControl.Label>Email</FormControl.Label>
+            <Input
+              autoCapitalize="none"
+              autoCorrect={false}
+              enablesReturnKeyAutomatically
+              keyboardType="email-address"
+              placeholder="Type your email"
+              returnKeyType="go"
+              variant="filled"
+            />
+          </FormControl>
+          <Button onPress={onSubmit}>Send</Button>
+        </VStack>
+      </VStack>
+    </ScrollView>
+  )
+}
